3e8efc302155f50e987b975b3ca258dcf0f113fc
Andy Lin <andy_lin@mail.ru>
2022-01-08 21:58:51 +0500

    import modules

---
+++ Best Calc/Best Calc.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.core.image import Image as CoreImage
+
+
+class Best_CaclApp(App):
+	def build():
+		pass
+
+
+if __name__ == '__main__':
+	App().run()
+
